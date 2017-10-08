4bd71872d8d81bb27f6a7026ce69299d9da0ca7d
shawnsya <32290891+shawnsya@users.noreply.github.com>
2017-10-09 01:41:19 +0800

    thinking part of the task

---
+++ taskddl10.9/judge.c
@@ -0,0 +1,15 @@
+//用switch判断范围
+//
+//假装我用了switch
+void judge(int n){
+	int a;
+	a = (n <= 2) ? 1 : 0;
+	switch(a){
+	case 1:
+		printf("0 <= n <= 2");
+	default:
+		printf("2 < n <=4");
+			
+	}
+	
+}
